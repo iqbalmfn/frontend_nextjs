994a6c5df1f1551d22b01a75da15c8a9b101cc05
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2022-09-19 13:21:28 +0700

    update

--- src/components/Layouts/Navigation.js
+++ src/components/Layouts/Navigation.js
@@ -37,6 +37,13 @@ const Navigation = ({ user }) => {
                 Dashboard
               </NavLink>
             </div>
+            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+              <NavLink
+                href="/books"
+                active={router.pathname === '/books'}>
+                Books
+              </NavLink>
+            </div>
           </div>
 
           {/* Settings Dropdown */}
